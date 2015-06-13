b0a50c71f13a33f2494bc04d7e856c2fe622ccd6
Mike Vaughan <dvaugha1@gmail.com>
2015-06-13 19:10:54 -0400

    All validations for cards are now finished

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,7 +4,12 @@ validates :name, presence: :true, uniqueness: :true
 validates :type, presence: :true
 validates :edition, presence: :true
 validates :rarity, presence: :true
-
+validates :artist, presence: :true
+validates :rule, presence: :true
+validates :flavor, presence: :true
+validates :mana, presence: :true
+validates :attack, presence: :true
+validates :toughness, presence: :true
 
 	def json_cards
     self.cards.map do |card|
